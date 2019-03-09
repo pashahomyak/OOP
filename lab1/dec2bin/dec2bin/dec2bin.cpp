744c86de455b3c132255b49a8a8ffbb0a6107e16
Admin <Admin@mail>
2019-03-09 12:37:19 +0300

    Добавил задания к lab1

---
+++ lab1/dec2bin/dec2bin/dec2bin.cpp
@@ -0,0 +1,68 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+const string INVALID_PARAMS = "Invalid arguments count\nUsage: dec2bin.exe <decimal number>";
+const string INVALID_INPUT = "Invalid decimal number\nWrite integer 0 < number < 2^32 - 1";
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		cout << INVALID_PARAMS << endl;
+		return 1;
+	}
+	
+	char *sign = argv[1];
+
+	if (sign[0] == '-')
+	{
+		cout << INVALID_INPUT << endl;
+		return 1;
+	}
+
+	istringstream stringStream(argv[1]);
+	string str = stringStream.str();
+	unsigned int num;
+
+	try
+	{
+		num = stoul(str);
+	}
+	catch (out_of_range)
+	{
+		cout << INVALID_INPUT << endl;
+		return 1;
+	}
+	catch (invalid_argument)
+	{
+		cout << INVALID_INPUT << endl;
+		return 1;
+	}
+
+	if (num == 0)
+	{
+		cout << "0" << endl;
+		return 0;
+	}
+
+	int integerPartOfNum, fractionalPartOfNum;
+	string binaryRes;
+
+	while (num != 0)
+	{
+		integerPartOfNum = num / 2;
+		fractionalPartOfNum = num % 2;
+		binaryRes += to_string(fractionalPartOfNum);
+		num = integerPartOfNum;
+	}
+
+	for (int i = binaryRes.length() - 1; i >= 0; i--)
+	{
+		cout << binaryRes[i];
+	}
+	cout << endl;
+}
---
+++ lab1/multmatrix/multmatrix/multmatrix.cpp
@@ -0,0 +1,140 @@
+﻿#include "pch.h"
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+const int DIMENSION = 3;
+
+bool CheckNumberOfSimbolsAfterComma(double num)
+{
+	int counter = 0;
+
+	stringstream stringStream;
+	stringStream << setprecision(15) << num;
+
+	string str = stringStream.str();
+
+	auto dotIndex = str.find(".");
+
+	if (dotIndex == str.npos)
+	{
+		return false;
+	}
+	else
+	{
+		counter = str.size() - 1 - dotIndex;
+	}
+
+	if (counter > 3)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool CheckEmptyFile(ifstream& file, char argv[])
+{
+	if (file.peek() == EOF)
+	{
+		cout << "File " << argv << " is empty" << endl;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool ChechOpenFile(ifstream& file, char argv[])
+{
+	if (!file.is_open())
+	{
+		cout << "Failed to found/open " << argv << " for reading" << endl;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void InitializeMatrix(ifstream& file1, ifstream& file2, double (&mat1)[DIMENSION][DIMENSION], double (&mat2)[DIMENSION][DIMENSION])
+{
+	for (int i = 0; i < DIMENSION; i++)
+	{
+		for (int j = 0; j < DIMENSION; j++)
+		{
+			file1 >> mat1[i][j];
+			file2 >> mat2[i][j];
+		}
+	}
+}
+
+void CalculateMatrix(double mat1[][DIMENSION], double mat2[][DIMENSION], double (&resMat)[DIMENSION][DIMENSION])
+{
+	for (int i = 0; i < DIMENSION; i++)
+	{
+		for (int j = 0; j < DIMENSION; j++)
+		{
+			for (int k = 0; k < DIMENSION; k++)
+			{
+				resMat[i][j] += mat1[i][k] * mat2[k][j];
+			}
+		}
+	}
+}
+
+void PrintMatrix(double mat[][DIMENSION])
+{
+	for (int i = 0; i < DIMENSION; i++)
+	{
+		for (int j = 0; j < DIMENSION; j++)
+		{
+			if (j == 1)
+			{
+				cout << " " << fixed << setprecision(3) << mat[i][j] << " ";
+			}
+			else
+			{
+				cout << fixed << setprecision(3) << mat[i][j];	
+			}
+		}
+		cout << endl;
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc != 3)
+	{
+		cout << "Invalid arguments count" << endl
+			 << "Usage:multmatrix.exe <matrixfile1> matrixfile2>" << endl;
+		return 1;
+	}
+
+	double matrix1[DIMENSION][DIMENSION];
+	double matrix2[DIMENSION][DIMENSION];
+	double resultMatrix[DIMENSION][DIMENSION] = { 0 };
+
+	ifstream matrixFile1(argv[1]);
+	ifstream matrixFile2(argv[2]);
+
+	if (ChechOpenFile(matrixFile1, argv[1]) || CheckEmptyFile(matrixFile1, argv[1]) || ChechOpenFile(matrixFile2, argv[2]) || CheckEmptyFile(matrixFile2, argv[2]))
+	{
+		return 1;
+	}
+
+	InitializeMatrix(matrixFile1, matrixFile2, matrix1, matrix2);
+	CalculateMatrix(matrix1, matrix2, resultMatrix);
+	PrintMatrix(resultMatrix);
+
+	matrixFile1.close();
+	matrixFile2.close();
+}
